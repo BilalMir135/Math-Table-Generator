ab47384ca6cc7e867b38be891b207aeff07f1385
Bilal Mir <43586901+BilalMir135@users.noreply.github.com>
2019-08-06 02:53:10 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Table
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxRange = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butGenerate = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.butClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // tbxNumber
+            // 
+            this.tbxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxNumber.Location = new System.Drawing.Point(36, 53);
+            this.tbxNumber.Name = "tbxNumber";
+            this.tbxNumber.Size = new System.Drawing.Size(341, 26);
+            this.tbxNumber.TabIndex = 0;
+            this.tbxNumber.TextChanged += new System.EventHandler(this.tbxNumber_TextChanged);
+            this.tbxNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxNumber_KeyPress);
+            this.tbxNumber.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbxNumber_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Range";
+            // 
+            // tbxRange
+            // 
+            this.tbxRange.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxRange.Location = new System.Drawing.Point(36, 125);
+            this.tbxRange.Name = "tbxRange";
+            this.tbxRange.Size = new System.Drawing.Size(341, 26);
+            this.tbxRange.TabIndex = 1;
+            this.tbxRange.TextChanged += new System.EventHandler(this.tbxNumber_TextChanged);
+            this.tbxRange.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxRange_KeyPress);
+            this.tbxRange.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbxNumber_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Table";
+            // 
+            // butGenerate
+            // 
+            this.butGenerate.Location = new System.Drawing.Point(529, 38);
+            this.butGenerate.Name = "butGenerate";
+            this.butGenerate.Size = new System.Drawing.Size(152, 58);
+            this.butGenerate.TabIndex = 2;
+            this.butGenerate.Text = "Generate";
+            this.butGenerate.UseVisualStyleBackColor = true;
+            this.butGenerate.Click += new System.EventHandler(this.butGenerate_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(36, 241);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(645, 308);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.TabStop = false;
+            // 
+            // butClear
+            // 
+            this.butClear.Location = new System.Drawing.Point(529, 125);
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(152, 58);
+            this.butClear.TabIndex = 3;
+            this.butClear.Text = "Clear";
+            this.butClear.UseVisualStyleBackColor = true;
+            this.butClear.Click += new System.EventHandler(this.butClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(707, 568);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.butClear);
+            this.Controls.Add(this.butGenerate);
+            this.Controls.Add(this.tbxRange);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxNumber);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Table Generation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxRange;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butGenerate;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button butClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Table
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butGenerate_Click(object sender, EventArgs e)
+        {
+            if (tbxNumber.Text == "")
+            {
+                Control ctrl = (Control)tbxNumber;
+                ctrl.BackColor = Color.LightPink;
+                MessageBox.Show("Please enter the number to generate table", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxNumber.Focus();
+            }
+            else if (tbxRange.Text == "")
+            {
+                Control ctrl = (Control)tbxRange;
+                ctrl.BackColor = Color.LightPink;
+                MessageBox.Show("Please enter the range to generate table", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbxRange.Focus();
+            }
+            else
+            {
+                double num = 0;
+                long range = 0;
+                try
+                {
+                    num = Convert.ToDouble(tbxNumber.Text);
+                    range = Convert.ToInt64(tbxRange.Text);
+                }
+                catch (FormatException)
+                {
+                    Control ctrl = (Control)tbxNumber;
+                    ctrl.BackColor = Color.LightPink;
+                    MessageBox.Show("Please enter the number in correct formate to generate table", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tbxNumber.Focus();
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("The entered data is out of the limit", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tbxNumber.Clear();
+                    tbxRange.Clear();
+                    tbxNumber.Focus();
+                }              
+                if (num == 0 ||range==0)
+                    return;
+                for(long i=0; i<= range; i++)
+                {
+                    listBox1.Items.Add(num + "   x   " + i + "   =   " + num * i);
+                }
+                
+            }
+        }
+
+        private void butClear_Click(object sender, EventArgs e)
+        {
+            tbxNumber.Clear();
+            tbxRange.Clear();
+            listBox1.Items.Clear();
+            tbxNumber.Focus();
+        }
+
+        private void tbxNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar) && (e.KeyChar!=46))
+                e.Handled = true;               
+        }
+
+        private void tbxRange_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void tbxNumber_TextChanged(object sender, EventArgs e)
+        {
+            Control ctrl = (Control)sender;
+            ctrl.BackColor = Color.White;
+        }
+
+        private void tbxNumber_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.SelectNextControl((Control)sender, true, true, false, true);
+            }
+        }
+    }
+}
